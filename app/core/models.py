5092d55ef1af9286d24974b6e9f227952a9783d8
Guillermo Brachetta <brachetta@me.com>
2020-11-18 14:51:01 +0100

    Added list ingredients feature

--- app/core/models.py
+++ app/core/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.conf import settings
+from django.db.models.deletion import CASCADE
 
 
 class UserManager(BaseUserManager):
@@ -50,6 +51,16 @@ class Tag(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient to be user in a recipe"""
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
 
     def __str__(self):
         return self.name
